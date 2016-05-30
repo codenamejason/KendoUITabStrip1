4f0f8d957f74d2cd602d1451fc520e399d1ed427
Jason Romero <codenamejason@outlook.com>
2016-05-29 23:36:33 -0400

    Add project files.

---
+++ KendoUITabStrip1/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+(function() {
+    var app = {
+        data: {}
+    };
+
+    var bootstrap = function() {
+        $(function() {
+            app.mobileApp = new kendo.mobile.Application(document.body, {
+                transition: 'slide',
+                skin: 'nova',
+                initial: 'components/homeView/view.html'
+            });
+        });
+    };
+
+    if (window.cordova) {
+        document.addEventListener('deviceready', function() {
+            if (navigator && navigator.splashscreen) {
+                navigator.splashscreen.hide();
+            }
+            bootstrap();
+        }, false);
+    } else {
+        bootstrap();
+    }
+
+    app.keepActiveState = function _keepActiveState(item) {
+        var currentItem = item;
+        $('#navigation-container li.active').removeClass('active');
+        currentItem.addClass('active');
+    };
+
+    window.app = app;
+
+    app.isOnline = function() {
+        if (!navigator || !navigator.connection) {
+            return true;
+        } else {
+            return navigator.connection.type !== 'none';
+        }
+    };
+}());
+
